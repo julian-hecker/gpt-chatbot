95b082f9d7c666866df08152719a912fb4fdc57f
Hecker <julianheckerdev@gmail.com>
2023-02-02 20:07:34 -0500

    clean up, add clearer prompts

--- functions/src/api/index.ts
+++ functions/src/api/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import compression from 'compression';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
@@ -21,30 +22,22 @@ export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());
+app.use(cookieParser());
 app.use(cors({ origin: true }));
 app.use(helmet());
 app.use(morgan('combined'));
-// app.use(decodeToken);
-
-app.get('/', (req, res) => {
-  res.sendStatus(200);
-
-  // const client = require('twilio')(accountSid, authToken);
-});
 
 app.post('/sms', async (req, res) => {
   const twiml = new MessagingResponse();
-  const { From, To, Body } = req.body;
+  const { From, Body } = req.body;
 
-  console.log(Body);
+  const history = ((req.cookies['__session'] as string) ?? '')
+    .split('\n')
+    .slice(-10)
+    .join('\n');
 
-  const prompt = `ML Tutor: I am a ML/AI language model tutor
-You: What is a language model?
-ML Tutor: A language model is a statistical model that describes the probability of a word given the previous words.
-You: What is a statistical model?
-`;
-
-  // how to deal with start_text, stop_text, stop, etc
+  const prompt = generatePrompt(Body, history);
+  console.log(prompt);
 
   try {
     const completion = await openai.createCompletion({
@@ -52,15 +45,21 @@ You: What is a statistical model?
       prompt,
       temperature: 0.9,
       presence_penalty: 0.6,
-      max_tokens: 150,
+      max_tokens: 80,
+      top_p: 1,
+      stop: ['Q: ', 'A: '],
       user: From,
     });
+    console.log(completion.data);
 
     const result = completion.data.choices[0].text;
-    console.log(completion.data);
     twiml.message(result || 'Sorry, please try again.');
+
+    const maxAge = 1000 * 60 * 60 * 24 * 365;
+    const newHistory = history + `\nQ: ${Body}\nA: ${result}`;
+    res.cookie('__session', newHistory, { maxAge });
   } catch (err) {
-    console.error((err as any)?.response?.data?.error?.message);
+    console.error(JSON.stringify(err));
     twiml.message(
       `Error: ${(err as any)?.response?.data?.error?.message}`,
     );
@@ -69,7 +68,15 @@ You: What is a statistical model?
   }
 });
 
-// app.use(router);
+function generatePrompt(message: string, history = '') {
+  return `I am an AI assistant. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with "Unknown." If I cannot help with a task, I will respond with "I can't do that."
 
-// app.use(notFoundHandler);
-// app.use(errorHandler);
+Q: Hello, who are you?
+A: I am an AI assistant.
+Q: Take me to the moon.
+A: I can't do that.
+Q: Who was the first human?
+A: Unknown.
+${history ? history + '\n' : ''}Q: ${message.trim()}
+A: `;
+}
